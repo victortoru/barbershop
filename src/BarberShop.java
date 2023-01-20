04025ecf0bd40e48ffc94afd98ca74cff4e47e68
victortoru <victortobaruelaruiz@elpuig.xeill.net>
2023-01-20 18:44:38 +0100

    dfhdf

--- src/BarberShop.java
+++ src/BarberShop.java
@@ -1,40 +1,42 @@
 import java.util.concurrent.Semaphore;
 
 public class BarberShop {
-    static int chairs = 5; // número de sillas de espera
-    static Semaphore barber = new Semaphore(0); // barbero
-    static Semaphore customer = new Semaphore(0); // cliente
-    static Semaphore accessSeats = new Semaphore(1); // acceso a las sillas
+    static int sillas = 5; // número de sillas de espera
+    static Semaphore barbero = new Semaphore(0); // barbero
+    static Semaphore cliente = new Semaphore(0); // cliente
+    static Semaphore sentarse = new Semaphore(1); // acceso a las sillas
 
-    static class Barber implements Runnable {
+    static Semaphore dormir = new Semaphore();
+
+    static class Barbero implements Runnable {
         public void run() {
             while (true) {
                 try {
-                    customer.acquire(); // el barbero espera a un cliente
-                    accessSeats.acquire(); // accede a las sillas
-                    chairs++; // una silla se libera
-                    System.out.println("Barber: Cutting hair. Chairs available: " + chairs);
-                    accessSeats.release();
-                    barber.release(); // el barbero comienza a cortar el cabello
+                    cliente.acquire(); // el barbero espera a un cliente
+                    sentarse.acquire(); // accede a las sillas
+                    sillas++; // una silla se libera
+                    System.out.println("El barbero está cortando el pelo, hay estas sillas disponibles " + chairs);
+                    sentarse.release();
+                    barbero.release(); // el barbero comienza a cortar el cabello
                 } catch (InterruptedException e) { }
             }
         }
     }
 
-    static class Customer implements Runnable {
+    static class Cliente implements Runnable {
         public void run() {
             while (true) {
                 try {
-                    accessSeats.acquire(); // accede a las sillas
-                    if (chairs > 0) {
-                        chairs--; // se sienta en una silla
-                        System.out.println("Customer: Sitting. Chairs available: " + chairs);
-                        customer.release(); // despierta al barbero
-                        accessSeats.release();
-                        barber.acquire(); // espera a que el barbero termine
+                    sentarse.acquire(); // accede a las sillas
+                    if (sillas > 0) {
+                        sillas--; // se sienta en una silla
+                        System.out.println("El cliente se sienta y hay estas sillas disponibles: " + chairs);
+                        cliente.release(); // despierta al barbero
+                        sentarse.release();
+                        barbero.acquire(); // espera a que el barbero termine
                     } else {
-                        System.out.println("Customer: Leaving. Chairs available: " + chairs);
-                        accessSeats.release(); // no hay sillas disponibles
+                        System.out.println("El cliente se va y quedan estas sillas disponibles " + chairs);
+                        sentarse.release(); // no hay sillas disponibles
                     }
                 } catch (InterruptedException e) { }
             }
@@ -42,9 +44,9 @@ public class BarberShop {
     }
 
     public static void main(String[] args) {
-        new Thread(new Barber()).start();
-        new Thread(new Customer()).start();
-        new Thread(new Customer()).start();
+        new Thread(new Barbero()).start();
+        new Thread(new Cliente()).start();
+        new Thread(new Cliente()).start();
         // puedes crear más hilos de clientes si lo deseas
     }
 }
