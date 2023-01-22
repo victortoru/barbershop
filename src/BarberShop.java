4607fe591e8ebe07d6acf7cbba4e7eefc8580b61
victortoru <113134993+victortoru@users.noreply.github.com>
2023-01-22 23:31:16 +0100

    Update BarberShop.java

--- src/BarberShop.java
+++ src/BarberShop.java
@@ -6,7 +6,6 @@ public class BarberShop {
     static Semaphore cliente = new Semaphore(0); // cliente
     static Semaphore sentarse = new Semaphore(1); // acceso a las sillas
 
-    static Semaphore dormir = new Semaphore();
 
     static class Barbero implements Runnable {
         public void run() {
@@ -15,7 +14,7 @@ public class BarberShop {
                     cliente.acquire(); // el barbero espera a un cliente
                     sentarse.acquire(); // accede a las sillas
                     sillas++; // una silla se libera
-                    System.out.println("El barbero está cortando el pelo, hay estas sillas disponibles " + chairs);
+                    System.out.println("El barbero está cortando el pelo, hay estas sillas disponibles " + sillas);
                     sentarse.release();
                     barbero.release(); // el barbero comienza a cortar el cabello
                 } catch (InterruptedException e) { }
@@ -30,12 +29,12 @@ public class BarberShop {
                     sentarse.acquire(); // accede a las sillas
                     if (sillas > 0) {
                         sillas--; // se sienta en una silla
-                        System.out.println("El cliente se sienta y hay estas sillas disponibles: " + chairs);
+                        System.out.println("El cliente se sienta y hay estas sillas disponibles: " + sillas);
                         cliente.release(); // despierta al barbero
                         sentarse.release();
                         barbero.acquire(); // espera a que el barbero termine
                     } else {
-                        System.out.println("El cliente se va y quedan estas sillas disponibles " + chairs);
+                        System.out.println("El cliente se va y quedan estas sillas disponibles " + sillas);
                         sentarse.release(); // no hay sillas disponibles
                     }
                 } catch (InterruptedException e) { }
